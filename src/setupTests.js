5ce6b53194bfd00113e2528b31b3eff34cc27e73
shivaniiiiie <shivanilattupally@gamail.com>
2024-01-16 15:05:50 +0000

    commit message

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
+import {configure as eConfigure} from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+eConfigure({adapter: new Adapter()})
